476d126654423b33593ac684237ceec612a46742
Emanuele <rosi.1812180@studenti.uniroma1.it>
2021-11-14 21:49:48 +0100

    fixing invariant mass

--- RandomDecay.cpp
+++ RandomDecay.cpp
@@ -1,6 +1,9 @@
 // Standard library header files
 #include <iostream>
 #include <cmath>
+#include <vector>
+#include <iomanip>
+#include <fstream>
 using std::cout;
 using std::endl;
 
@@ -11,6 +14,8 @@ using std::endl;
 #include "TFile.h"
 #include "TLorentzVector.h"
 #include "TMath.h"
+#include "TTree.h"
+#include "TString.h"
 #include "TStyle.h"
 #include "TGaxis.h"
 
@@ -20,7 +25,7 @@ using std::endl;
 #define m_pi 0.140 // Pion mass (GeV)
 #define m_K 0.500 // Pion mass (GeV)
 #define Nevents 10000 // Numebr of random events generated
-#define sigma_resolution 0.03 // Gaussian resolution of detector. This number is the value of std deviation
+#define sigma_resolution 0.1 // Gaussian resolution of detector. This number is the value of std deviation
 
 int main(){
 
@@ -40,9 +45,10 @@ int main(){
 
     // The three histograms instances (name, title, number of bins, range)
     int nbins = 100;
-    TH1F invariant_m("Invariant Mass", "Distribution of invariant mass of #pi + K after the decay", nbins, 4.5, 6);
+    TH1F invariant_m("Invariant Mass", "Distribution of invariant mass of #pi + K after the decay", nbins, 2, 8);
     TH1F opening_angle("Opening Angle","Distribution of opening angle between #pi and K in LAB", nbins, 3, 3.2);
-    TH1F measured_m("Measured Invariant mass","Distribution of measured invariant mass of #pi + K after the decay", nbins, 4, 12);
+    TH1F measured_m("Measured Invariant mass","Distribution of measured invariant mass of #pi + K after the decay", nbins, 2, 8);
+
 
 // DEFINITION OF FOUR MOMENTA
 
@@ -57,7 +63,15 @@ int main(){
     // Momentum of generated particles in the CoM system 
     double p_star = sqrt(pow(m_B,4) + pow(m_pi,4) + pow(m_K,4) - 2*pow(m_K,2)*pow(m_pi,2) - 2*pow(m_K,2)*pow(m_B,2) - 2*pow(m_pi,2)*pow(m_B,2) )/(2*m_B);
     cout << ">>> Value of momentum of produced particles in the ceneter of mass system is\t p* = " << p_star << "\n" << endl;
-    
+
+// TTREE SETTINGS
+
+    TTree* myTree = new TTree("datatree", "tree containing our data");
+    myTree->Branch("p_B", &p_B, "momentum of B");
+    int nDau = 2;
+    myTree->Branch("nDau", &nDau, "boh");
+    /*** continue with all branches... ***/
+
 // RANDOM MOMENTA GENERATION
 
     cout << ">>> Generating random spherical distribution for pion and K meson momenta.\n" << endl;
@@ -103,25 +117,22 @@ int main(){
         p_K_meas = gen->Gaus(p4_K_0.P(), p4_K_0.P()*sigma_resolution);
 
         // Definitions of measured momenta inf the LAB reference frame
-        p4_pi_meas.SetPtEtaPhiM(p_pi_meas, p4_pi_0.Eta(), p4_pi_0.Phi(), m_pi);
-        p4_K_meas.SetPtEtaPhiM(p_K_meas, p4_K_0.Eta(), p4_K_0.Phi(), m_K);
+        p4_pi_meas.SetPtEtaPhiE(p_pi_meas, p4_pi_0.Eta(), p4_pi_0.Phi(), sqrt(m_pi*m_pi + p_pi_meas*p_pi_meas));
+        p4_K_meas.SetPtEtaPhiE(p_K_meas, p4_K_0.Eta(), p4_K_0.Phi(), sqrt(m_K*m_K + p_K_meas*p_K_meas));
 
         // Measured invariant masses of the two particles in LAB, involving smearing effect
         p4_tot = p4_pi_meas + p4_K_meas;
         // Filling the 3rd hist with measured invariant masses
         measured_m.Fill(p4_tot.M());
 
-        /*if (i%1000 == 0){
-            # Print something to check the correct behaviour of the program
-        }*/
-
     }
 
 //PLOTTING AND SAVING RESULTS
 
-    // True inavriant mass of the system in the LAB
-    // Set canvas
+    // Set canvas for plotting
     TCanvas canv1("canv", "canvas for plotting", 1500, 1280);
+    
+    // True inavriant mass of the system in the LAB
     // Set axis labels
     invariant_m.GetXaxis()->SetTitle("Invariant mass for #pi + K [GeV]");
     invariant_m.GetYaxis()->SetTitle("Number of events");
@@ -130,11 +141,8 @@ int main(){
     invariant_m.SetLineColor(kGray+2);
     invariant_m.Draw();
     canv1.SaveAs("./true-mass.pdf");
-    invariant_m.Write();
 
     // Opening angle between pion and kaon in the LAB
-    // Set canvas
-    TCanvas canv2("canv", "canvas for plotting", 1500, 1280);
     // Set axis labels
     opening_angle.GetXaxis()->SetTitle("Opening angle [rad]");
     opening_angle.GetYaxis()->SetTitle("Number of events");
@@ -142,40 +150,31 @@ int main(){
     opening_angle.SetLineColor(kOrange+9);
     opening_angle.SetFillColor(kOrange+6);
     opening_angle.Draw();
-    canv2.SaveAs("./opening-angle.pdf");
-    opening_angle.Write();
+    canv1.SaveAs("./opening-angle.pdf");
 
     // Measured invariant mass of the system in the LAB
-    // Set canvas
-    TCanvas canv3("canv", "canvas for plotting", 1500, 1280);
     // Set axis labels
     measured_m.GetXaxis()->SetTitle("Invariant mass for #pi + K [GeV]");
     measured_m.GetYaxis()->SetTitle("Number of events");
     // Draw and save
     measured_m.SetLineColor(kGreen+3);
     measured_m.Draw("pe");
-    canv3.SaveAs("./measured-mass.pdf");
-    measured_m.Write();
+    canv1.SaveAs("./measured-mass.pdf");
 
-/******************************* NEED TO UNDERSTAND HOW TO SUPERPOSE TWO PLOTS *******************************/
-/*
     // True and measured invariant mass of the system in LAB in one graph
-    // Set canvas
-    TCanvas canv4("canv", "canvas for plotting", 1280, 1280);
-    // Split the pdf into two parts
-    canv4.Divide(1,2);
+    canv1.Divide(1,2);
     // Set axis labels
     measured_m.GetXaxis()->SetTitle("Invariant mass for #pi + K [GeV]");
     measured_m.GetYaxis()->SetTitle("Number of events");
     // Draw first plot
-    canv4.cd(1);
+    canv1.cd(1);
     invariant_m.Draw();
     // Draw second plot
-    canv4.cd(2);
+    canv1.cd(2);
     measured_m.Draw("pe");
     // Save
-    canv4.SaveAs("./invariant-mass.pdf");
-*/
+    canv1.SaveAs("./invariant-mass.pdf");
+
 
 //CLOSING THINGS AND DELETING OBJECTS
 
